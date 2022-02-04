35c3afeab68a63c6ba4a790827235c08f66002f5
Jamie Gardner <jamie@thatgardnerone.com>
2022-02-04 17:37:00 +0000

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,19 @@
-npm run build;
-git add dist;
-git commit -m 'Committed via deploy script';
-git subtree push --prefix=dist origin gh-pages;
+#!/usr/bin/env sh
 
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:GardnerOne/GardnerOne.github.io.git main
+
+cd -
